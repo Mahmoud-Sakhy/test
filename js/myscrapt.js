7b41ccff4fc5670071289d8f1bf4b0dfe8fbd63d
Mahmoud-Sakhy <119065926+Mahmoud-Sakhy@users.noreply.github.com>
2023-05-04 12:03:13 +0300

    Add files via upload

---
+++ js/myscrapt.js
@@ -0,0 +1,16 @@
+function myFunction() {
+  var element = document.querySelector(".modal");
+  if (element.style.display === "flex") {
+    element.style.display = "flex";
+  } else {
+    element.style.display = "none";
+  }
+}
+// EventButton.addEventListener("click", function () {
+//   var element = document.querySelector(".modal");
+//   if (element.style.display === "flex") {
+//     element.style.display = "flex";
+//   } else {
+//     element.style.display = "none";
+//   }
+// });
